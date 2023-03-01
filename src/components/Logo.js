a10dcd6f8fb87cbd2689b1200c2e97b0881655f3
tommaso.cisternino <tommycisternino@gmail.com>
2023-03-01 20:54:08 +0100

    styling

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,12 @@
-import {Image} from "react-bootstrap";
+import { Image } from "react-bootstrap";
 
-function Logo() {
-    return (
-        <Image src={process.env.REACT_APP_PUBLIC_URL + "money-logo.svg"} />
-    );
+function Logo({ maxWidth = null }) {
+  return (
+    <Image
+      src={process.env.REACT_APP_PUBLIC_URL + "money-logo.svg"}
+      style={maxWidth ? { maxWidth: maxWidth } : {}}
+    />
+  );
 }
 
 export default Logo;
--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -1,6 +1,7 @@
 import AuthContext from "../contexts/AuthContext";
 import { useContext } from "react";
 import AccessMethod from "../components/AccessMethod";
+import Logo from "../components/Logo";
 
 function AccountPage() {
   const authContext = useContext(AuthContext);
@@ -8,14 +9,21 @@ function AccountPage() {
   if (authContext) {
     if (authContext.user) {
       return (
-        <div className={"col-10 offset-1 mt-5"}>
-          <p>
-            Ciao {authContext.user.username}, <br />
-            grazie per aver scaricato questa repository. <br />
-            Questa WebApp è ancora in fase di sviluppo. Puoi darmi suggerimenti
-            su come migliorare le mie skills in React e Laravel sulle relative
-            repository GitHub.
-          </p>
+        <div className={"container h-100"}>
+          <div className={"row"}>
+            <div className={"col-10 offset-1 mt-5"}>
+              <div className={"row justify-content-center"}>
+                <Logo maxWidth={300} />
+              </div>
+              <div className={"row"}>
+                Ciao {authContext.user.username}, <br />
+                grazie per aver scaricato questa repository. <br />
+                Questa WebApp è ancora in fase di sviluppo. Puoi darmi
+                suggerimenti su come migliorare le mie skills in React e Laravel
+                sulle relative repository GitHub.
+              </div>
+            </div>
+          </div>
         </div>
       );
     }
