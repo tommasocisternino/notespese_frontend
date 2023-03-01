8b9136b5c8e2edc52e8e226a9c2fc45f49838316
tommaso.cisternino <tommycisternino@gmail.com>
2023-03-01 20:32:48 +0100

    styling

--- src/pages/AccountPage.js
+++ src/pages/AccountPage.js
@@ -1,20 +1,26 @@
 import AuthContext from "../contexts/AuthContext";
-import {useContext} from "react";
+import { useContext } from "react";
 import AccessMethod from "../components/AccessMethod";
 
 function AccountPage() {
-    const authContext = useContext(AuthContext);
+  const authContext = useContext(AuthContext);
 
-    if (authContext) {
-        if (authContext.user) {
-            return (
-                <div className={"col-auto"}>
-                    account
-                </div>
-            );
-        }
+  if (authContext) {
+    if (authContext.user) {
+      return (
+        <div className={"col-10 offset-1 mt-5"}>
+          <p>
+            Ciao {authContext.user.username}, <br />
+            grazie per aver scaricato questa repository. <br />
+            Questa WebApp è ancora in fase di sviluppo. Puoi darmi suggerimenti
+            su come migliorare le mie skills in React e Laravel sulle relative
+            repository GitHub.
+          </p>
+        </div>
+      );
     }
-    return (<AccessMethod/>);
+  }
+  return <AccessMethod />;
 }
 
 export default AccountPage;
--- src/pages/AddMovementPage.js
+++ src/pages/AddMovementPage.js
@@ -46,7 +46,7 @@ function ListaMovimentiPage() {
   };
 
   return (
-    <div className={"container h-100"}>
+    <>
       <CategoryModal
         isCreating={isCreating}
         setIsCreating={setIsCreating}
@@ -60,182 +60,182 @@ function ListaMovimentiPage() {
         show={deleteModalShow}
         setShow={setDeleteModalShow}
       />
-      <div className={"row"}>
-        <div className={"col-12"}>
-          <Form onSubmit={handleSubmit}>
-            <div className={"row mt-5 pb-5 text-center"}>
-              <h1>AGGIUNGI MOVIMENTO</h1>
-            </div>
-            <div className={"row mt-3"}>
-              <div className={"col-10 offset-1"}>
-                <hr />
-                <div className={"row justify-content-around d-flex"}>
-                  <div className={"col-10"}>
-                    <Form.Label className={"fw-bolder"}>
-                      Tipo di movimento
-                    </Form.Label>
-                    <Form.Select
-                      size="sm"
-                      onChange={(e) => {
-                        setType(e.target.value);
-                      }}
-                      defaultValue={"-"}
-                    >
-                      <option value={"-"}>USCITA</option>
-                      <option value={"+"}>ENTRATA</option>
-                    </Form.Select>
-                  </div>
-                </div>
+      <div className={"container h-100"}>
+        <div className={"row"}>
+          <div className={"col-12"}>
+            <Form onSubmit={handleSubmit}>
+              <div className={"row mt-5 pb-5 text-center"}>
+                <h1>AGGIUNGI MOVIMENTO</h1>
               </div>
-            </div>
-            <div className={"row mt-3"}>
-              <div className={"col-10 offset-1"}>
-                <div className={"row justify-content-around d-flex"}>
-                  <div className={"col-10"}>
-                    <Form.Label className={"fw-bolder"}>
-                      Categoria
-                      <Button
-                        variant={"primary"}
-                        type={"button"}
-                        className={"mx-2"}
-                        onClick={() => {
-                          setIsCreating(true);
-                          setModalShow(true);
+              <div className={"row mt-3"}>
+                <div className={"col-10 offset-1"}>
+                  <hr />
+                  <div className={"row justify-content-around d-flex"}>
+                    <div className={"col-10"}>
+                      <Form.Label className={"fw-bolder"}>
+                        Tipo di movimento
+                      </Form.Label>
+                      <Form.Select
+                        size="sm"
+                        onChange={(e) => {
+                          setType(e.target.value);
                         }}
+                        defaultValue={"-"}
                       >
-                        <PlusIcon width={16} height={16} />
-                      </Button>
-                      {category && category != 0 ? (
-                        <>
-                          <Button
-                            variant={"danger"}
-                            type={"button"}
-                            className={"mx-2"}
-                            onClick={() => {
-                              setDeleteModalShow(true);
-                            }}
-                          >
-                            <MinusIcon width={16} height={16} />
-                          </Button>
-                          <Button
-                            variant={"warning"}
-                            type={"button"}
-                            className={"mx-2"}
-                            onClick={() => {
-                              setModalShow(true);
-                            }}
-                          >
-                            <PencilIcon width={16} height={16} />
-                          </Button>
-                        </>
-                      ) : (
-                        <></>
-                      )}
-                    </Form.Label>
-                    <AuthContext.Consumer>
-                      {({ categories }) =>
-                        categories && (
-                          <Form.Select
-                            size="sm"
-                            onChange={(e) => {
-                              if (e.target.value != 0) {
-                                setCategory({
-                                  id: e.target.value,
-                                  name: e.target.selectedOptions[0].dataset
-                                    .text,
-                                  color:
-                                    e.target.selectedOptions[0].dataset.color,
-                                });
-                              } else {
-                                setCategory(null);
-                              }
-                            }}
-                            defaultValue={null}
-                          >
-                            <option value={0} key={"option_cat_null"}>
-                              Seleziona una categoria...
-                            </option>
-                            {categories.map((cat, index) => {
-                              return (
-                                <option
-                                  value={cat.id}
-                                  key={"option_cat_" + index}
-                                  data-text={cat.name}
-                                  data-color={cat.color}
-                                >
-                                  {cat.name}
-                                </option>
-                              );
-                            })}
-                          </Form.Select>
-                        )
-                      }
-                    </AuthContext.Consumer>
-                    {errors && errors.category_id && (
-                      <p className={"text-danger text-center"}>
-                        {errors.category_id[0]}
-                      </p>
-                    )}
+                        <option value={"-"}>USCITA</option>
+                        <option value={"+"}>ENTRATA</option>
+                      </Form.Select>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className={"row mt-3"}>
-              <div className={"col-5 offset-1"}>
-                <Form.Group>
-                  <FormControl
-                    changeSetter={setDate}
-                    label_text={"Data movimento"}
-                    label_className={"fw-bolder"}
-                    type={"date"}
-                    value={date}
-                    errors={errors && errors.date ? errors.date : null}
-                  />
-                </Form.Group>
+              <div className={"row mt-3"}>
+                <div className={"col-10 offset-1"}>
+                  <div className={"row justify-content-around d-flex"}>
+                    <div className={"col-10"}>
+                      <Form.Label className={"fw-bolder"}>
+                        Categoria
+                        <Button
+                          variant={"primary"}
+                          type={"button"}
+                          className={"mx-2"}
+                          onClick={() => {
+                            setIsCreating(true);
+                            setModalShow(true);
+                          }}
+                        >
+                          <PlusIcon width={16} height={16} />
+                        </Button>
+                        {category && category != 0 ? (
+                          <>
+                            <Button
+                              variant={"danger"}
+                              type={"button"}
+                              className={"mx-2"}
+                              onClick={() => {
+                                setDeleteModalShow(true);
+                              }}
+                            >
+                              <MinusIcon width={16} height={16} />
+                            </Button>
+                            <Button
+                              variant={"warning"}
+                              type={"button"}
+                              className={"mx-2"}
+                              onClick={() => {
+                                setModalShow(true);
+                              }}
+                            >
+                              <PencilIcon width={16} height={16} />
+                            </Button>
+                          </>
+                        ) : (
+                          <></>
+                        )}
+                      </Form.Label>
+                      <AuthContext.Consumer>
+                        {({ categories }) =>
+                          categories && (
+                            <Form.Select
+                              size="sm"
+                              onChange={(e) => {
+                                if (e.target.value != 0) {
+                                  setCategory({
+                                    id: e.target.value,
+                                    name: e.target.selectedOptions[0].dataset
+                                      .text,
+                                    color:
+                                      e.target.selectedOptions[0].dataset.color,
+                                  });
+                                } else {
+                                  setCategory(null);
+                                }
+                              }}
+                              defaultValue={null}
+                            >
+                              <option value={0} key={"option_cat_null"}>
+                                Seleziona una categoria...
+                              </option>
+                              {categories.map((cat, index) => {
+                                return (
+                                  <option
+                                    value={cat.id}
+                                    key={"option_cat_" + index}
+                                    data-text={cat.name}
+                                    data-color={cat.color}
+                                  >
+                                    {cat.name}
+                                  </option>
+                                );
+                              })}
+                            </Form.Select>
+                          )
+                        }
+                      </AuthContext.Consumer>
+                      {errors && errors.category_id && (
+                        <p className={"text-danger text-center"}>
+                          {errors.category_id[0]}
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className={"col-4 offset-1"}>
-                <Form.Group >
-                  <FormControl
-                    changeSetter={setValue}
-                    label_text={"Importo"}
-                    type={"number"}
-                    value={value}
-                    placeholder={"IMPORTO"}
-                    label_className={"fw-bolder"}
-                    errors={errors && errors.value ? errors.value : null}
-                  />
-                </Form.Group>
+              <div className={"row mt-3"}>
+                <div className={"col-5 offset-1"}>
+                  <Form.Group>
+                    <FormControl
+                      changeSetter={setDate}
+                      label_text={"Data movimento"}
+                      label_className={"fw-bolder"}
+                      type={"date"}
+                      value={date}
+                      errors={errors && errors.date ? errors.date : null}
+                    />
+                  </Form.Group>
+                </div>
+                <div className={"col-4 offset-1"}>
+                  <Form.Group>
+                    <FormControl
+                      changeSetter={setValue}
+                      label_text={"Importo"}
+                      type={"number"}
+                      value={value}
+                      placeholder={"IMPORTO"}
+                      label_className={"fw-bolder"}
+                      errors={errors && errors.value ? errors.value : null}
+                    />
+                  </Form.Group>
+                </div>
               </div>
-            </div>
-            <div className={"row mt-3"}>
-              <div className={"col-10 offset-1"}>
-                <Form.Group
-                  className="mb-3"
+              <div className={"row mt-3"}>
+                <div className={"col-10 offset-1"}>
+                  <Form.Group className="mb-3">
+                    <Form.Label className={"fw-bolder"}>Note</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={4}
+                      onChange={(e) => {
+                        setNote(e.target.value);
+                      }}
+                    />
+                  </Form.Group>
+                </div>
+              </div>
+              <div className={"row mt-5"}>
+                <Button
+                  variant={"success"}
+                  className={"col-4 offset-4"}
+                  type={"submit"}
                 >
-                  <Form.Label className={"fw-bolder"}>Note</Form.Label>
-                  <Form.Control
-                    as="textarea"
-                    rows={4}
-                    onChange={(e) => {
-                      setNote(e.target.value);
-                    }}
-                  />
-                </Form.Group>
+                  SALVA
+                </Button>
               </div>
-            </div>
-            <div className={"row mt-5"}>
-              <Button
-                variant={"success"}
-                className={"col-4 offset-4"}
-                type={"submit"}
-              >
-                SALVA
-              </Button>
-            </div>
-          </Form>
+            </Form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
